43fb0eb9f0139b64dfc0971537ec88273939b83e
Robin <robin.lenz@5minds.de>
2018-12-11 14:33:01 +0100

    :recycle: log to console

--- Program.cs
+++ Program.cs
@@ -23,17 +23,6 @@ namespace IdentityServer
         {
             return WebHost.CreateDefaultBuilder(args)
                     .UseStartup<Startup>()
-                    .UseSerilog((context, configuration) =>
-                    {
-                        configuration
-                            .MinimumLevel.Debug()
-                            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                            .MinimumLevel.Override("System", LogEventLevel.Warning)
-                            .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-                            .Enrich.FromLogContext()
-                            .WriteTo.File(@"identityserver4_log.txt")
-                            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate);
-                    })
                     .UseKestrel(options => options.ConfigureHTTPS())
                     .Build();
         }
