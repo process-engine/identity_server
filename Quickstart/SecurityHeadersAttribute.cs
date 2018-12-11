98ecb4f1e1f3827d7af01523c6a55187b3823dfb
Robin <robin.lenz@5minds.de>
2018-12-11 16:53:54 +0100

    :bug: frame-ancestors removed

--- Quickstart/SecurityHeadersAttribute.cs
+++ Quickstart/SecurityHeadersAttribute.cs
@@ -26,8 +26,9 @@ namespace IdentityServer4.Quickstart.UI
                     context.HttpContext.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
                 }
 
+                //// !!!!!!!!!!!!!! frame-ancestors 'none'; entfernt  !!!!!!!!!!!!!!!!!!
                 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
-                var csp = "default-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';";
+                var csp = "default-src 'self'; object-src 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';";
                 // also consider adding upgrade-insecure-requests once you have HTTPS in place for production
                 //csp += "upgrade-insecure-requests;";
                 // also an example if you need client images to be displayed from twitter
