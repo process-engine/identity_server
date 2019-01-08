0402f9fc56e2f78bda8b141b106a80391ed0e196
Robin <robin.lenz@5minds.de>
2019-01-08 15:39:08 +0100

    :bug: fix discovery document

--- Startup.cs
+++ Startup.cs
@@ -18,159 +18,166 @@ using ProcessEngine.IdentityServer.Web.Features;
 
 namespace IdentityServer
 {
-  public class Startup
-  {
-    public IConfiguration Configuration { get; }
-    public IHostingEnvironment Environment { get; }
-
-    public Startup(IConfiguration configuration, IHostingEnvironment environment)
+    public class Startup
     {
-      var currentDirectory = Directory.GetCurrentDirectory();
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(currentDirectory)
-          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-          .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
-          .AddEnvironmentVariables();
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
 
-      Configuration = builder.Build();
-      Environment = environment;
-    }
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(currentDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
 
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddTransient<IClaimCheck, ClaimCheck>();
+            Configuration = builder.Build();
+            Environment = environment;
+        }
 
-      services.AddDbContext<IdentityExpressDbContext>(options =>
-      {
-        options.UseSqlite(Configuration.GetConnectionString("Users"));
-      });
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IClaimCheck, ClaimCheck>();
 
-      services.AddIdentity<IdentityExpressUser, IdentityExpressRole>(options =>
-      {
-        options.Password.RequireDigit = false;
-        options.Password.RequiredLength = 5;
-        options.Password.RequireLowercase = false;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = false;
-      })
-          .AddUserStore<IdentityExpressUserStore>()
-          .AddRoleStore<IdentityExpressRoleStore>()
-          .AddIdentityExpressUserClaimsPrincipalFactory()
-          .AddDefaultTokenProviders();
+            services.AddDbContext<IdentityExpressDbContext>(options =>
+            {
+                options.UseSqlite(Configuration.GetConnectionString("Users"));
+            });
 
-      services.AddMvc();
+            services.AddIdentity<IdentityExpressUser, IdentityExpressRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+                .AddUserStore<IdentityExpressUserStore>()
+                .AddRoleStore<IdentityExpressRoleStore>()
+                .AddIdentityExpressUserClaimsPrincipalFactory()
+                .AddDefaultTokenProviders();
 
-      services.Configure<IISOptions>(iis =>
-      {
-        iis.AuthenticationDisplayName = "Windows";
-        iis.AutomaticAuthentication = false;
-      });
+            services.AddMvc();
 
-      var connectionString = Configuration.GetConnectionString("Configuration");
-      var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            services.Configure<IISOptions>(iis =>
+            {
+                iis.AuthenticationDisplayName = "Windows";
+                iis.AutomaticAuthentication = false;
+            });
 
-      var builder = services.AddIdentityServer(options =>
-      {
-        options.Events.RaiseErrorEvents = true;
-        options.Events.RaiseInformationEvents = true;
-        options.Events.RaiseFailureEvents = true;
-        options.Events.RaiseSuccessEvents = true;
-      })
-          .AddAspNetIdentity<IdentityExpressUser>()
-          // this adds the config data from DB (clients, resources, CORS)
-          .AddConfigurationStore(options =>
-          {
-            options.ConfigureDbContext = db =>
-                      db.UseSqlite(connectionString,
-                          sql => sql.MigrationsAssembly(migrationsAssembly));
-          })
-          // // this adds the operational data from DB (codes, tokens, consents)
-          .AddOperationalStore(options =>
-          {
-            options.ConfigureDbContext = db =>
-                      db.UseSqlite(connectionString,
-                          sql => sql.MigrationsAssembly(migrationsAssembly));
+            var connectionString = Configuration.GetConnectionString("Configuration");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
-            // this enables automatic token cleanup. this is optional.
-            options.EnableTokenCleanup = true;
-            // options.TokenCleanupInterval = 15; // interval in seconds. 15 seconds useful for debugging
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+                .AddAspNetIdentity<IdentityExpressUser>()
+                // this adds the config data from DB (clients, resources, CORS)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = db =>
+                        db.UseSqlite(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                // // this adds the operational data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = db =>
+                        db.UseSqlite(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+              // this enables automatic token cleanup. this is optional.
+              options.EnableTokenCleanup = true;
+              // options.TokenCleanupInterval = 15; // interval in seconds. 15 seconds useful for debugging
           });
 
-      if (Environment.IsDevelopment())
-      {
-        builder.AddDeveloperSigningCredential();
-      }
-      else
-      {
-        builder.AddDeveloperSigningCredential();
-      }
-
-      services.AddAuthentication()
-          .AddGoogle(options =>
-          {
-            options.ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com";
-            options.ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh";
-          });
-
-      var MicrosoftClientId = Configuration["MICROSOFT_CLIENT_ID"];
-      var MicrosoftClientSecret = Configuration["MICROSOFT_CLIENT_SECRET"];
-
-      if (!String.IsNullOrEmpty(MicrosoftClientId) && !String.IsNullOrEmpty(MicrosoftClientSecret))
-      {
-        services.AddAuthentication()
-            .AddMicrosoftAccount(options =>
+            if (Environment.IsDevelopment())
             {
-              options.ClientId = MicrosoftClientId;
-              options.ClientSecret = MicrosoftClientSecret;
-            });
-      }
-
-      var FacebookClientId = Configuration["FACEBOOK_CLIENT_ID"];
-      var FacebookClientSecret = Configuration["FACEBOOK_CLIENT_SECRET"];
-
-      if (!String.IsNullOrEmpty(FacebookClientId) && !String.IsNullOrEmpty(FacebookClientSecret))
-      {
-        services.AddAuthentication()
-            .AddFacebook(options =>
+                builder.AddDeveloperSigningCredential();
+            }
+            else
             {
-              options.ClientId = FacebookClientId;
-              options.ClientSecret = FacebookClientSecret;
+                builder.AddDeveloperSigningCredential();
+            }
+
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com";
+                    options.ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh";
+                });
+
+            var MicrosoftClientId = Configuration["MICROSOFT_CLIENT_ID"];
+            var MicrosoftClientSecret = Configuration["MICROSOFT_CLIENT_SECRET"];
+
+            if (!String.IsNullOrEmpty(MicrosoftClientId) && !String.IsNullOrEmpty(MicrosoftClientSecret))
+            {
+                services.AddAuthentication()
+                    .AddMicrosoftAccount(options =>
+                    {
+                        options.ClientId = MicrosoftClientId;
+                        options.ClientSecret = MicrosoftClientSecret;
+                    });
+            }
+
+            var FacebookClientId = Configuration["FACEBOOK_CLIENT_ID"];
+            var FacebookClientSecret = Configuration["FACEBOOK_CLIENT_SECRET"];
+
+            if (!String.IsNullOrEmpty(FacebookClientId) && !String.IsNullOrEmpty(FacebookClientSecret))
+            {
+                services.AddAuthentication()
+                    .AddFacebook(options =>
+                    {
+                        options.ClientId = FacebookClientId;
+                        options.ClientSecret = FacebookClientSecret;
+                    });
+            }
+
+            services.UseAdminUI();
+
+            services.AddDataProtection();
+        }
+
+        public void Configure(IApplicationBuilder appBuilder)
+        {
+            appBuilder.UseAdminUI();
+            appBuilder.Map("/identityserver", app =>
+            {
+
+                if (Environment.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                    app.UseDatabaseErrorPage();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                }
+
+                app.Use((context, next) =>
+              {
+                      context.Request.Scheme = "https";
+                      return next();
+                  });
+
+                app.UseCors(builder =>
+                    builder.WithOrigins("http://localhost:9000", "http://localhost:17290")
+                           .AllowAnyHeader()
+                           .AllowAnyMethod()
+                           .AllowCredentials()
+                    );
+
+                app.UseDefaultFiles();
+                app.UseStaticFiles();
+                app.UseIdentityServer();
+                app.UseAuthentication();
+                app.UseMvcWithDefaultRoute();
             });
-      }
-
-      services.UseAdminUI();
-
-      services.AddDataProtection();
-    }
-
-    public void Configure(IApplicationBuilder appBuilder)
-    {
-      appBuilder.UseAdminUI();
-      appBuilder.Map("/identityserver", app =>
-      {
-
-        if (Environment.IsDevelopment())
-        {
-          app.UseDeveloperExceptionPage();
-          app.UseDatabaseErrorPage();
         }
-        else
-        {
-          app.UseExceptionHandler("/Home/Error");
-        }
-        app.UseCors(builder =>
-                  builder.WithOrigins("http://localhost:9000", "http://localhost:17290")
-                         .AllowAnyHeader()
-                         .AllowAnyMethod()
-                         .AllowCredentials()
-                  );
-
-        app.UseDefaultFiles();
-        app.UseStaticFiles();
-        app.UseIdentityServer();
-        app.UseAuthentication();
-        app.UseMvcWithDefaultRoute();
-      });
     }
-  }
 }
